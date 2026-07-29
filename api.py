84370f6fd24af4a303cbbcfaf2fffea6144b8260
woominju0208 <alswn5033@gmail.com>
2026-07-30 00:37:41 +0900

    배포 주소 로컬에서 변경

--- api.py
+++ api.py
@@ -54,7 +54,8 @@ _allowed_origins = [o for o in os.getenv("ALLOWED_ORIGINS", "").split(",") if o]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=_allowed_origins,
-    allow_origin_regex=r"http://(localhost|127\.0\.0\.1):\d+",
+    # allow_origin_regex=r"http://(localhost|127\.0\.0\.1):\d+",
+    allow_origin_regex=r"https://food-company-sales-1.onrender.com/",
     allow_methods=["POST"],
     allow_headers=["*"],
 )
